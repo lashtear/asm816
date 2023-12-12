834a5c94ccb0b535369da1645abc87b88c6ccded
Emily Backes <lashtear@gmail.com>
2023-12-11 17:58:27 -0800

    fixed verbose, more arg parsing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ pub mod cpu816;
 
 use clap::Parser;
 use log::*;
+use std::fs::{File, OpenOptions};
+use std::io;
 use std::{path::PathBuf, sync::Arc};
 
 //use chumsky::prelude::*;
@@ -28,7 +30,7 @@ enum Command {
         /// Set output file
         output_file: PathBuf,
         #[arg(short = 'g', default_value = "false")]
-        /// Enable debug mode
+        /// Include debug records
         enable_debug: bool,
         /// Source files to be assembled
         input_files: Vec<PathBuf>,
@@ -39,7 +41,7 @@ enum Command {
         /// Set output file
         output_file: PathBuf,
         #[arg(short = 'g', default_value = "false")]
-        /// Enable debug mode
+        /// Include debug records
         enable_debug: bool,
         /// Object files to be linked
         input_files: Vec<PathBuf>,
@@ -104,8 +106,77 @@ pub enum AsmAST {
     Operation { name: String, args: Vec<AsmExpr> },
 }
 
+// like LevelFilter::from_usize, but not private to that module
+fn level_filter_from_usize(u: usize) -> Option<LevelFilter> {
+    match u {
+        0 => Some(LevelFilter::Off),
+        1 => Some(LevelFilter::Error),
+        2 => Some(LevelFilter::Warn),
+        3 => Some(LevelFilter::Info),
+        4 => Some(LevelFilter::Debug),
+        5 => Some(LevelFilter::Trace),
+        _ => None,
+    }
+}
+
+/// Safely open an output file
+fn open_output(filename: &PathBuf) -> io::Result<File> {
+    OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(filename)
+}
+
+/// Warn the user about non-existent include paths
+fn check_include_path(includes: &Vec<PathBuf>) {
+    for path in includes {
+        if !path.exists() {
+            warn!("Include path ({path:?}) does not exist");
+        } else if !path.is_dir() {
+            warn!("Include path ({path:?}) is not a directory");
+        }
+    }
+}
+
 fn main() {
-    env_logger::init();
     let args = Args::parse();
-    info!("Received arguments:\n{:#?}", args);
+    // Log at WARN + number of verboses.
+    // Overriden by RUST_LOG if present.
+    env_logger::init_from_env(
+        env_logger::Env::new().default_filter_or(
+            level_filter_from_usize(args.verbose as usize + 2)
+                .or_else(|| Some(LevelFilter::Trace))
+                .unwrap()
+                .as_str(),
+        ),
+    );
+    debug!("Received arguments:\n{:#?}", args);
+    match args.command {
+        Command::Assemble {
+            include_paths,
+            output_file,
+            enable_debug: _,
+            input_files,
+        } => {
+            if input_files.is_empty() {
+                error!("No input files specified");
+            } else {
+                check_include_path(&include_paths);
+                let outfile = open_output(&output_file).expect("output file");
+                outfile.sync_all().expect("successfully written");
+            }
+        }
+        Command::Link {
+            output_file,
+            enable_debug: _,
+            input_files,
+        } => {
+            if input_files.is_empty() {
+                error!("No input files specified");
+            } else {
+                let outfile = open_output(&output_file).expect("output file");
+                outfile.sync_all().expect("successfully written");
+            }
+        }
+    }
 }
